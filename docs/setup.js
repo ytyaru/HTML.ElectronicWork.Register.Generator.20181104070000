1f7b426fe15f656394fe12c2fc89dcde08ebd956
ytyaru <ytyaru@outlook.jp>
2018-11-04 08:32:46 +0900

    抵抗器系列の算出を追加

--- docs/setup.js
+++ docs/setup.js
@@ -1,8 +1,17 @@
 window.addEventListener('DOMContentLoaded', function() {
-	SetupRegisterForm();
+	SetupRegisterSystemForm();
 });
-function SetupRegisterForm() {
-	CalcRegisterValue();
+function SetupRegisterSystemForm() {
+	document.forms.RegisterSystemForm.RegisterSystemExponential.focus();
+	var rse = document.getElementById("RegisterSystemExponential");
+	rse.addEventListener("change", CalcRegisterSystemValue);
+	rse.addEventListener("keyup", CalcRegisterSystemValue);
+	rse.addEventListener("paste", CalcRegisterSystemValue);
+	rse.addEventListener("input", CalcRegisterSystemValue);
+	CalcRegisterSystemValue();
 }
-function CalcRegisterValue() {
+function CalcRegisterSystemValue() {
+	var RegisterSystemValue = 3 * (2 ** document.forms.RegisterSystemForm.RegisterSystemExponential.value)
+	var target = document.getElementById("RegisterSystemValue");
+	target.value = RegisterSystemValue;
 }
